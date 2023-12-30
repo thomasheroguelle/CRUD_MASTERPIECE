0f89a69fc1f66031859596def486580581f84174
thomasheroguelle <totosh93270@gmail.com>
2023-12-30 21:25:19 +0100

    fix endpoints + add CorsConfig folder

---
+++ src/main/java/com/masterpiecebrouillon/brouillon/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.masterpiecebrouillon.brouillon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/com/masterpiecebrouillon/brouillon/controller/UserController.java
+++ src/main/java/com/masterpiecebrouillon/brouillon/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/v1/user")
 public class UserController {
     @Autowired
